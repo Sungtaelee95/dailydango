ec66802e08554f182e6acd4b3ad88ae3b28af09d
sungtae-lee <120715142+Sungtaelee95@users.noreply.github.com>
2026-03-18 16:40:27 +0900

    update: 릴리즈 버전 업그레이드

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ extensions.configure<ApplicationExtension> {
 
     defaultConfig {
         applicationId = "com.bhst.dailydango"
-        versionCode = 7
-        versionName = "1.0.7"
+        versionCode = 8
+        versionName = "1.0.8"
     }
 
     buildTypes {
