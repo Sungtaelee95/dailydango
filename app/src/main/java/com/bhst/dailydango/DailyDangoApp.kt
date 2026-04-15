70129d6256eb62cf4371b01c144a45d3c66551be
sungtae-lee <120715142+Sungtaelee95@users.noreply.github.com>
2026-04-15 17:29:21 +0900

    feat: 광고 콘텐츠 제한 설정 기능 추가

--- app/src/main/java/com/bhst/dailydango/DailyDangoApp.kt
+++ app/src/main/java/com/bhst/dailydango/DailyDangoApp.kt
@@ -5,6 +5,7 @@ import coil.ImageLoader
 import com.google.android.gms.ads.MobileAds
 import coil.ImageLoaderFactory
 import coil.decode.ImageDecoderDecoder
+import com.google.android.gms.ads.RequestConfiguration
 import dagger.hilt.android.HiltAndroidApp
 
 
@@ -13,6 +14,14 @@ class DailyDangoApp: Application(), ImageLoaderFactory {
     override fun onCreate() {
         super.onCreate()
         MobileAds.initialize(this)
+        val requestConfiguration = RequestConfiguration.Builder()
+            // 1. 광고 콘텐츠 등급을 'G(전체이용가)'로 강제 고정
+            .setMaxAdContentRating(RequestConfiguration.MAX_AD_CONTENT_RATING_G)
+            // 2. 아동 온라인 개인정보 보호법(COPPA)에 따라 어린이를 대상으로 처리하도록 태그 설정
+            .setTagForChildDirectedTreatment(RequestConfiguration.TAG_FOR_CHILD_DIRECTED_TREATMENT_TRUE)
+            .build()
+
+        MobileAds.setRequestConfiguration(requestConfiguration)
     }
     override fun newImageLoader(): ImageLoader {
         return ImageLoader.Builder(this)
--- feature/home/src/main/java/com/bhst/dailydango/home/HomeTabScreen.kt
+++ feature/home/src/main/java/com/bhst/dailydango/home/HomeTabScreen.kt
@@ -15,24 +15,15 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices.TABLET
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.bhst.dailydango.ad_mob.loadInterstitialAd
-import com.bhst.dailydango.ad_mob.showInterstitialAd
 import com.bhst.dailydango.app.feature.home.R
 import com.bhst.dailydango.designsystem.theme.DailyDangoTheme
-import com.google.android.gms.ads.interstitial.InterstitialAd
 
 @Composable
 fun HomeTabScreen(
